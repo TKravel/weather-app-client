2226acaeae2b9b70c478d10655ef905c910f1d3a
Tim Kravel <TKravel@Yahoo.com>
2022-01-11 08:50:20 -0500

    css for hero text

--- src/components/LandingPage.jsx
+++ src/components/LandingPage.jsx
@@ -3,10 +3,10 @@ import React from 'react';
 const LandingPage = () => {
 	return (
 		<div id='landing-page'>
-			<div id='hero-text'>
-				<h1>Forecasts for you</h1>
+			<div id='hero-text-container'>
+				<h1 className='hero-text'>Forecasts for you</h1>
 				{/* <p>Don't get caught by surprise</p> */}
-				<p>- CURRENT - HOURLY - THREE-DAY -</p>
+				<p className='hero-text'>- CURRENT - HOURLY - THREE-DAY -</p>
 			</div>
 		</div>
 	);
